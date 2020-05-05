8d4004bb5c04dff7e70351a3aa30312bf109fe4b
Ubuntu <ubuntu@ip-172-31-16-8.us-east-2.compute.internal>
2020-05-05 22:43:38 +0000

    second line added

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 class Test{
         Public static void main(String[] args){
          System.out.println("Test program");
+	 System.out.println("Added second line");
          }
          }
          
